3fafe1bc26d5a7292027f44594da610e32e9b7f2
tallesnicacio <43962500+tallesnicacio@users.noreply.github.com>
2020-07-02 08:42:54 -0300

    Add files via upload

---
+++ routes/pages.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Router = require('express');
+
+const router = express.Router();
+
+router.get('/', (req,res) => {
+    res.render('index');
+});
+
+router.get('/register', (req,res) => {
+    res.render('register');
+});
+
+router.get('/login', (req,res) => {
+    res.render('login');
+});
+
+router.get('/quiz', (req,res) => {
+    res.render('quiz');
+});
+
+module.exports = router;
